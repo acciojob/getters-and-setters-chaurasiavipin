f97e382ca67a689d6120760817adbae960c10467
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-13 16:04:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 //complete this code
 class Person {
 	constructor(name, age){
-   this._name=name;
+		this._name=name;
 		this._age=age;
-	
+		
 	}
 	get name(){
+		return this._name;
+	}
+	set age(age){
+		this._age=age;
+	}
 
-	return this._name;
 	}
-	set age(){
- this._age=age;
-	}
-}
+	
+	
+
 
 class Student extends Person {
 	study(){
 		console.log(`${this._name} is studying`);
-		
 	}
+	
 }
 
 class Teacher extends Person {
 	teach(){
-console.log(`${this._name} is teaching`);
+		console.log(`${this._name} is teaching`)
 	}
 }
 
